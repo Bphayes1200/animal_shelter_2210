f5ff4c409aa0a18ec321500e48422cfd3f7b1ac2
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-12 10:50:55 -0600

    almost complete iteration1

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,21 @@
 class Shelter
-end
+    attr_reader :name, :capacity, :pets
+    def initialize(shelter, num_of_animals)
+        @name = shelter
+        @capacity = num_of_animals
+        @pets = []
+    end
+  
+    def add_pet(pet_name)
+     @pets.push(pet_name)
+    end
+  
+    def call_pets
+        names_exclamation = []
+        @pets.each do |pet|
+            @pets. << names_exclamation
+        end
+    end
+  
+ end
+ 
